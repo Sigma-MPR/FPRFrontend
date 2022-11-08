66478e37db7e87d256376ec994dc3a061dfcf440
Karan Banjade <karan.banjade@gmail.com>
2022-11-08 15:27:05 +0530

    fixed navbar logic

--- src/pages/Shared Components/Navbar.js
+++ src/pages/Shared Components/Navbar.js
@@ -5,14 +5,16 @@ import {MdAccountCircle} from 'react-icons/md'
 import {IoIosLogOut} from 'react-icons/io'
  // this page needs to be rendered properly and icons need to be resized
 export default function Navbar(){
-    return(
-        <div className="h-auto flex justify-between p-3">
-            <img src={SurajmalLogo} className="w-12"/>
-            
-            <div className="py-4 flex">
-            <AiFillHome/> 
-            <MdAccountCircle/>
-            <IoIosLogOut/>
+    if( document.location.href.split("/")[document.location.href.split("/").length-1]!=='login')
+        return(
+            <div className="h-auto flex justify-between p-3">
+                <img src={SurajmalLogo} className="w-12"/>
+                
+                <div className="py-4 flex">
+                <AiFillHome/> 
+                <MdAccountCircle/>
+                <IoIosLogOut/>
+                </div>
             </div>
-        </div>
-    )}
+        )
+    }
