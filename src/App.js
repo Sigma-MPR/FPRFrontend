bf063bcd2f1dea1e73a407d77ad6587a2e869059
Karan Banjade <karan.banjade@gmail.com>
2022-11-08 15:27:31 +0530

    setup routes

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import Navbar from '../src/pages/Shared Components/Navbar.js'
 function App() {
   return (
     <div>
-
-    
     <Navbar />
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<LoginPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path = "*" element={<LoginPage />} />
+          <Route path = "/login">
+            <Route index element={<LoginPage />} />
+            <Route path = "*" element = {<LoginPage/>} />
+          </Route>
+          <Route path = "dashboard" element = {<h1>dashboard Route</h1>} />
+          <Route path = "profile" element = {<h1>profile Route</h1>}>
+              <Route path = "edit" element = {<h1>profile edit Route</h1>} />
+              <Route index element = {<h1>profile Route</h1>}/>
+          </Route>
+
+          <Route index element = {<h1>Index Route</h1>} />
         </Route>
       </Routes>
     </BrowserRouter>
