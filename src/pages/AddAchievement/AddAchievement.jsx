f5b87dbe62d5af1d67579cd54673d31d36b6bb0a
Karan Banjade <karan.banjade@gmail.com>
2022-11-08 21:51:24 +0530

    web version

--- src/pages/AddAchievement/AddAchievement.jsx
+++ src/pages/AddAchievement/AddAchievement.jsx
@@ -24,7 +24,7 @@ const CP = {
 const textField = (fieldName) => {
     return (
         `<div>
-                <label for=${fieldName} className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">${fieldName}</label>
+                <label for=${fieldName} className="block mb-2 text-sm text-gray-900 dark:text-gray-300">${fieldName}</label>
                 <input type="text" id=${fieldName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required="" />
         </div>`
     )
@@ -33,7 +33,7 @@ const textField = (fieldName) => {
 const numberField = (fieldName) => {
     return (
             `<div>
-                <label for=${fieldName} className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">${fieldName}</label>
+                <label for=${fieldName} className="block mb-2 text-sm text-gray-900 dark:text-gray-300">${fieldName}</label>
                 <input type="number" id=${fieldName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
             </div>`
     )
@@ -41,7 +41,7 @@ const numberField = (fieldName) => {
 const dateField = (fieldName) => {
     return (
             `<div>
-                <label for=${fieldName} className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">${fieldName}</label>
+                <label for=${fieldName} className="block mb-2 text-sm font-black text-gray-900 dark:text-gray-400">${fieldName}</label>
                 <input type="date" id=${fieldName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
             </div>`
     )
