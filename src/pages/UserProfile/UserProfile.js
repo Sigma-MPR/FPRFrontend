f286856c3604abf10e1178caad0dd8fd81066ded
Karnika <karnikarajput1122@gmail.com>
2022-12-01 13:23:40 +0530

    user profile created

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -4,8 +4,8 @@ import ProfilePhoto from "../Images/ProfilePhoto.png"
 // "/profile"
 export default function UserProfile() {
     return (
-        <div className="grid md:grid-cols-2 sm:grid-cols-1 sm:py-20 md:py-28 bg-slate-400">
-            <div>
+        <div className="grid md:grid-cols-2 sm:grid-cols-1 py-16 m-5 rounded-2xl shadow-xl bg-slate-100">
+            <div className="py-5">
                 <div className=" flex justify-center">
                     <img src={ProfilePhoto} className="rounded-full w-48" />
                 </div>
@@ -16,22 +16,34 @@ export default function UserProfile() {
                     <h1>Tester</h1>
                 </div>
             </div>
-            <div className="">
-                <div>
-                    <p>Email:</p>
+            <div className="w-5/6 py-10 items-center">
+                <div className="">
+                    <div className="grid grid-cols-2">
+                        <p>Email:</p>
+                        <p>abc@gmail.com</p>
+                    </div>
+
+                    <div className="grid grid-cols-2">
+                        <p>Department:</p>
+                        <p>xyz</p>
+                    </div>
+
+                    <div className="grid grid-cols-2">
+                        <p>Shift:</p>
+                        <p>M</p>
+                    </div>
+
+                    <div className="grid grid-cols-2">
+                        <p>Qualification:</p>
+                        <p>PhD</p>
+                    </div>
+                </div>
+                <div className="grid sm:grid-cols-3 grid-cols-1 my-5">
+                    <button class="btn btn-outline btn-sm m-2 p-0">Analytics</button>
+                    <button class="btn btn-outline btn-sm m-2 p-0">Edit Profile</button>
+                    <button class="btn btn-outline btn-sm m-2 p-0">Change Password</button>
                 </div>
 
-                <div>
-                    <p>Department:</p>
-                </div>
-
-                <div>
-                    <p>Shift:</p>
-                </div>
-
-                <div>
-                    <p>Qualification:</p>
-                </div>
 
             </div>
         </div>
