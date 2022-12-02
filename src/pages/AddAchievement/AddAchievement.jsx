deb907b2cb6c116fecd2e722c772aa288e9d1b7c
tarnibalgoher05 <tarnibalgoher@gmail.com>
2022-12-02 23:16:26 +0530

    css added

--- src/pages/AddAchievement/AddAchievement.jsx
+++ src/pages/AddAchievement/AddAchievement.jsx
@@ -6,24 +6,24 @@ import { AchievementSchemaData } from "../../constants";
 const textField = (fieldName) => {
     return (
         `<div>
-                <label for=${fieldName} className="block mb-2 text-sm text-gray-900 dark:text-gray-300">${fieldName}</label>
-                <input type="text" id=${fieldName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required="" />
+                <label for=${fieldName} className="block mb-2 text-sm text-gray-900 dark:text-gray-400 ">${fieldName}</label>
+                <input type="text" id=${fieldName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md" placeholder="John" required="" />
         </div>`
     )
 }
 const numberField = (fieldName) => {
     return (
             `<div>
-                <label for=${fieldName} className="block mb-2 text-sm text-gray-900 dark:text-gray-300">${fieldName}</label>
-                <input type="number" id=${fieldName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
+                <label for=${fieldName} className="block mb-2 text-sm text-gray-900 dark:text-gray-400">${fieldName}</label>
+                <input type="number" id=${fieldName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md" placeholder="" required="" />
             </div>`
     )
 }
 const dateField = (fieldName) => {
     return (
             `<div>
-                <label for=${fieldName} className="block mb-2 text-sm font-black text-gray-900 dark:text-gray-400">${fieldName}</label>
-                <input type="date" id=${fieldName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
+                <label for=${fieldName} className="block mb-2 text-sm font-black text-gray-900 dark:text-gray-400 ">${fieldName}</label>
+                <input type="date" id=${fieldName} className="bg-gray-50 border border-gray-450 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md"  placeholder="" required="" />
             </div>`
     )
 }
@@ -60,8 +60,8 @@ const AddAchievement = () => {
     <form class = "w-1/2">
         <div className="grid gap-3 mb-3 md:grid-cols-1" id="addAchievement">
             <div className="add-achievement">            
-                <label for="achievement" className="inline mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Choose Achievement Type</label>
-                <select id="achievement" className="bg-gray-50 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange = {changeOption}>
+                <label for="achievement" className="inline mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 ">Choose Achievement Type</label>
+                <select id="achievement" className="bg-gray-50 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 " onChange = {changeOption}>
                 {AchievementDropDown[prop]||<option>Choose Type Of Achievement</option>}
                 {
                     Object.keys(AchievementDropDown).map((item) => {
@@ -77,7 +77,7 @@ const AddAchievement = () => {
             </div>
             <div id="fields" className="add-achievement"></div>   
         </div>
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <button type="submit" className="text-white bg-blue-900 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 ">SUBMIT</button>
     </form>
 </div>
 
