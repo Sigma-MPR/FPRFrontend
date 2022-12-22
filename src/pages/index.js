a7b64b70b3461d2ff353a851fe6876a10bba1621
Karan Banjade <karan.banjade@gmail.com>
2022-12-22 23:04:43 +0530

    rebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export {default as LoginPage} from './LoginPage/LoginPage';
 export {default as AddAchievement} from './AddAchievement/AddAchievement';
 export {default as Dashboard} from './Dashboard/Dashboard';
 export {default as ViewSingleAchievementList} from './ViewSingleAchievementList/ViewSingleAchievementList';
+export {default as UserProfile} from './UserProfile/UserProfile';
