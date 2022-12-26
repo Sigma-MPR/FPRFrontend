7615cc420061d3bb24cb7907cda9d1e592afc339
Karan Banjade <karan.banjade@gmail.com>
2022-12-26 20:12:39 +0530

    fix: conflict

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { LoginPage, AddAchievement, Dashboard, ViewSingleAchievementList  } from './pages';
+import { LoginPage, AddAchievement, Dashboard, ViewSingleAchievementList } from './pages';
 import Loader from './components/Loader/Loader'
 import Navbar from '../src/pages/Shared Components/Navbar.js'
 function App() {
   return (
     <div>
-    <Navbar />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route path = "login">
-            <Route index element={<LoginPage />} />
-            <Route path = "*" element = {<LoginPage/>} />
-          </Route>  
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="login">
+              <Route index element={<LoginPage />} />
+              <Route path="*" element={<LoginPage />} />
+            </Route>
 
-          <Route path = "dashboard" element = {<Dashboard/>} />
-          
-          <Route path = "profile" element = {<h1>profile Route</h1>}>
-              <Route path = "edit" element = {<h1>profile edit Route</h1>} />
-              <Route index element = {<h1>profile Route</h1>}/>
-          </Route>
-          <Route path = "achievements">
-              <Route path = "add" element = {<h1>achievements add Route</h1>} />
-              <Route path = "*" element = {<ViewSingleAchievementList />} />
-          </Route>
+            <Route path="dashboard" element={<Dashboard />} />
 
-          <Route path = "addachievement" element = {<AddAchievement />} />
-          <Route path = "viewachievement" element = {<ViewSingleAchievementList />} />
-          <Route index element = {<Loader />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            <Route path="profile" element={<h1>profile Route</h1>}>
+              <Route path="edit" element={<h1>profile edit Route</h1>} />
+              <Route index element={<h1>profile Route</h1>} />
+            </Route>
+            <Route path="achievements">
+              <Route path="add" element={<h1>achievements add Route</h1>} />
+              <Route path="*" element={<ViewSingleAchievementList />} />
+            </Route>
+
+            <Route path="addachievement" element={<AddAchievement />} />
+            <Route path="viewachievement" element={<ViewSingleAchievementList />} />
+            <Route index element={<Loader />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
