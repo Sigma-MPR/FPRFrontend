9ec518ed9aeb9d89688a1b2c48e477a72f30b8c4
Karan Banjade <karan.banjade@gmail.com>
2023-01-03 17:52:04 +0530

    css for addachiement

--- src/pages/AddAchievement/AddAchievement.jsx
+++ src/pages/AddAchievement/AddAchievement.jsx
@@ -49,12 +49,11 @@ const AddAchievement = () => {
     }
     return (
         <div className="w-1/2 ml-auto mr-auto">
-            <form className="w-1/2">
                 <div className="grid gap-3 mb-3 md:grid-cols-1" id="addAchievement">
                     <div className="add-achievement">
                         <label htmlFor="achievement" className="inline mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 ">Choose Achievement Type</label>
 
-                        <select id="achievement" className="bg-gray-50 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 " onChange={changeOption}>
+                        <select id="achievement" className="bg-gray-50 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit p-2.5 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 " onChange={changeOption}>
                             {!AchievementDropDown[prop] && <option>Choose Type Of Achievement</option>}
                             {
                                 Object.keys(AchievementDropDown).map((item, index) => {
@@ -68,12 +67,11 @@ const AddAchievement = () => {
                         </select>
                     </div>
                     {Loading&&<Loader />}
-                    <form id="fields" className="add-achievement">
-                    </form>
                 </div>
+                <form id="fields" className="add-achievement">
+                    </form>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value = {achievement} 
                 onClick={(e)=>addAndToast(e, tempMap[achievement])}>Submit</button>
-            </form>
             <CustomToastContainer/>
         </div>
 
