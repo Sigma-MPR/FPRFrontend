e1f92b90080b2b7dd68c47cda6ab8fc65314aa01
Tarni Balgoher <tarnibalgoher@gmail.com>
2023-01-03 22:24:38 +0530

    dashboard css

--- src/pages/AddAchievement/AddAchievement.jsx
+++ src/pages/AddAchievement/AddAchievement.jsx
@@ -27,7 +27,7 @@ const AddAchievement = () => {
 
     useEffect(() => {
         document.querySelector("#fields").innerHTML = '';
-        setLoading(true);
+        setLoading(true); 
         makefields(AchievementSchemaData, setLoading)
         // ToastSuccess("Input Modal Made");
     }, [AchievementSchemaData]);
@@ -48,9 +48,9 @@ const AddAchievement = () => {
             <form className="w-1/2">
                 <div className="grid gap-3 mb-3 md:grid-cols-1" id="addAchievement">
                     <div className="add-achievement">
-                        <label htmlFor="achievement" className="inline mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 ">Choose Achievement Type</label>
+                        <label htmlFor="achievement" className="inline mb-3 text-md  px-2.5 py-2.5 font-medium text-gray-900 dark:text-gray-400 ">CHOOSE ACHIEVEMENT TYPE</label>
 
-                        <select id="achievement" className="bg-gray-50 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 " onChange={changeOption}>
+                        <select id="achievement" className="bg-gray-50 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 " onChange={changeOption}>
                             {!AchievementDropDown[prop] && <option>Choose Type Of Achievement</option>}
                             {
                                 Object.keys(AchievementDropDown).map((item, index) => {
@@ -65,10 +65,11 @@ const AddAchievement = () => {
                     </div>
                     {Loading&&<Loader />}
                     <div id="fields" className="add-achievement">
+                        <div id="sub-fields" className="achievement-field"></div>
                     </div>
                 </div>
-                <button type="submit" className="submit-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value = {achievement} 
-                onClick={(e)=>ToastPromise(addAchievementApiFunction(e, tempMap[achievement]))}>Submit</button>
+                <button type="submit" className="submit-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md w-full sm:w-auto px-4 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value = {achievement} 
+                onClick={(e)=>ToastPromise(addAchievementApiFunction(e, tempMap[achievement]))}>SUBMIT</button>
             </form>
             <CustomToastContainer/>
         </div>
--- src/pages/AddAchievement/functions.js
+++ src/pages/AddAchievement/functions.js
@@ -6,23 +6,24 @@ import { useLocation } from "react-router";
 const textField = (fieldName, required) => {
     return (
         `<div>
-                <label for=${fieldsMapping[fieldName]} className="block mb-2 text-sm text-gray-900 dark:text-gray-400 ">${fieldsMapping[fieldName]}</label>
-                <input type="text" id=${fieldsMapping[fieldName]} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md" placeholder='Enter ${fieldsMapping[fieldName]}' ${required && 'required'} />
+                <label for=${fieldsMapping[fieldName]} className="text-label block mb-2 text-md text-gray-900 dark:text-gray-400 ">${fieldsMapping[fieldName]}</label>
+                <input type="text" id=${fieldsMapping[fieldName]} className="textField bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md" placeholder='Enter ${fieldsMapping[fieldName]}' ${required && 'required'} />
         </div>`
     )
 }
 const numberField = (fieldName, required) => {
     return (
         `<div>
-                <label for=${fieldsMapping[fieldName]} className="block mb-2 text-sm text-gray-900 dark:text-gray-400">${fieldsMapping[fieldName]}</label>
-                <input type="number" id=${fieldsMapping[fieldName]} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md" placeholder='Enter ${fieldsMapping[fieldName]}' required  />
+                <label for=${fieldsMapping[fieldName]} className="numfield-div block mb-2 text-sm text-gray-900 dark:text-gray-400">${fieldsMapping[fieldName]}</label> 
+                <input type="number" id=${fieldsMapping[fieldName]} className="numField bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md" placeholder='Enter ${fieldsMapping[fieldName]}' required  />
             </div>`
     )
 }
+
 const dateField = (fieldName, required) => {
     return (
         `<div>
-                <label for=${fieldsMapping[fieldName]} className="block mb-2 text-sm font-black text-gray-900 dark:text-gray-400 ">${fieldsMapping[fieldName]}</label>
+                <label for=${fieldsMapping[fieldName]} className="datefeild-div block mb-2 text-sm font-black text-gray-900 dark:text-gray-400 ">${fieldsMapping[fieldName]}</label>
                 <input type="date" id=${fieldsMapping[fieldName]} className="bg-gray-50 border border-gray-450 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md"  ${required && 'required'} />
             </div>`
     )
