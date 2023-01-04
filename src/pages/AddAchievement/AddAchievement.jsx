777e2b30dd2db9f494a6be1adf876654b5fd00dc
Tarni Balgoher <tarnibalgoher@gmail.com>
2023-01-05 05:20:17 +0530

    form css changes

--- src/pages/AddAchievement/AddAchievement.jsx
+++ src/pages/AddAchievement/AddAchievement.jsx
@@ -71,8 +71,10 @@ const AddAchievement = () => {
 
                     </div>
                 </div>
-                <button type="submit" className="submit-btn text-white bg-blue-700 hover:bg-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-2xl text-md w-full sm:w-auto px-4 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value = {achievement} 
+                <div className="btn-div">
+                <button type="submit" className="submit-btn text-white bg-blue-700 hover:bg-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md w-full sm:w-auto px-8 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value = {achievement} 
                 onClick={(e)=>ToastPromise(addAchievementApiFunction(e, tempMap[achievement]))}>SUBMIT</button>
+                </div>
             </form>
            
             <CustomToastContainer/>
