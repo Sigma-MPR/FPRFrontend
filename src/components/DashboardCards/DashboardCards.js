c6bd60d7587bbe26a918a06d3d1300d89e507e8a
Tarni Balgoher <tarnibalgoher@gmail.com>
2023-01-05 20:30:36 +0530

    dashboard color scheme

--- src/components/DashboardCards/DashboardCards.js
+++ src/components/DashboardCards/DashboardCards.js
@@ -6,11 +6,11 @@ export default function DashboardCards(props) {
 
     return (
         <span id="DashboardCard" className={props.available ? "show" : "soon"} title={props.available ? props.name : "Feature Coming Soon"} disabled>
-            <div className="dashboardCard w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-7 dark:bg-gray-800 dark:border-gray-700">
+            <div className="dashboardCard w-full max-w-sm p-4 bg-lavender border rounded-lg shadow-md sm:p-7 dark:bg-gray-800 dark:border-gray-700">
                 <Link to={props.available ? props.link : "/"} className="DashboardCardLink" state={{prop: props}}> 
-                    <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Achievement Category</h5>
-                    <div className="flex items-baseline text-gray-900 dark:text-white pb-1">
-                        <span className="text-2x1 font-semibold ml-2 content-center	"  >
+                    <h5 className=" mb-4 font-semibold text-blue">Achievement Category</h5>
+                    <div className="flex items-baseline pb-1">
+                        <span className="text-black leading-4 text-lg font-semibold ml-2 mb-2 content-center" >
                            
                             {props.name}</span>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,51 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('@tailwindcss/forms'),
+//     require("daisyui"),
+//   ],
+// }
+
+
+/**@type {import('tailwindcss').Config}  */ 
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
+    colors: {
+      'light-beige': '#fdf8f6',
+      'blue':"#1C1258",
+      'red':'#EE4166',
+      'pink':'#EABFCB',
+      'light': '#CACAEA',
+      'dark': '#293747',
+      'Mustard': '#FF9B00',
+      'yellow': "#FFD28C",
+      'lavender':'#BAB7CD',
+      'black':'#000',
+      
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require("daisyui"),
+    require('daisyui'),
   ],
-}
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+  
+          },
+        }
+      ],
+    },
+    }
+
