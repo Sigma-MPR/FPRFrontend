c94be7b6ca185de89497250b79bd3331cea149f9
Tarni Balgoher <tarnibalgoher@gmail.com>
2023-01-07 11:30:29 +0530

    achivement css

--- src/pages/AddAchievement/AddAchievement.jsx
+++ src/pages/AddAchievement/AddAchievement.jsx
@@ -76,7 +76,7 @@ const AddAchievement = () => {
                    
                 </div>
                 <div className="btn-div mt-0">
-                <button type="submit" className="submit-btn text-white bg-blue-700 hover:bg-800 focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-md  mt-0 w-full sm:w-auto px-8 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value = {achievement} 
+                <button type="submit" className="submit-btn text-white bg-blue hover:bg-800 focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-md  mt-0 w-full sm:w-auto px-8 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value = {achievement} 
                 onClick={(e)=>ToastPromise(addAndToast(e, tempMap[achievement]))}>SUBMIT</button>
                 </div>
             </form>
