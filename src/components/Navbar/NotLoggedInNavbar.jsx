a104154e07130035c27685e997e3254c67808e1a
Karan Banjade <karan.banjade@gmail.com>
2023-01-07 12:27:32 +0530

    navbar complete

--- src/components/Navbar/NotLoggedInNavbar.jsx
+++ src/components/Navbar/NotLoggedInNavbar.jsx
@@ -1,19 +1,27 @@
 const NotLoggedInNavbar = () => {
-    return (
-        <div className="navbar fixed"style={{
-  backgroundColor: "rgba(255, 255, 255, 0.9)",
-        }}>
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">FPR</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Sign Up</a></li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-</div>
-    )
+  return (
+    <div className="navbar fixed" style={{
+      backgroundColor: "rgba(255, 255, 255, 0.9)",
+    }}>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">FPR</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li><a href="#" onClick={(e) => {
+            e.preventDefault()
+            window.location.href = "/signup"
+          }}>Sign Up</a></li>
+          <li><a href="#" onClick={
+            (e) => {
+              e.preventDefault()
+              window.location.href = "/"
+            }
+          }>About Us</a></li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default NotLoggedInNavbar
